59fca359f7ee80ca7a0261795821de37fe5bd7bb
nad2ia <ramadaninadia04@gmail.com>
2023-11-04 23:57:33 +0700

    CekLogin

---
+++ lib/CekLogin.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart'; 
+import 'CekLogin.dart';
+
+class extend StatefulWidget {
+  @override
+  CekLogin createStates() => CekLogin();
+}
+class CekLogin extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        padding: const EdgeInsets.all(8),
+        color: Colors.lightBlue,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              width: 100,
+              height: 100,
+              decoration: BoxDecoration(
+                color: Colors.black87,
+                shape: BoxShape.circle
+              ),
+              child: Center(
+                child: Icon(Icons.person, size: 50, color: Colors.white,),
+              ),
+             ),
+
+             SizedBox(height: 20,),
+
+             Text("Selamat Datang, Silahkan Masuk", style: TextStyle(fontSize: 20, color: Colors.black87),),
+
+             TextFormField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.black87)
+                ),
+                prefixIcon: Icon(Icons.person, size: 40,),
+                hintText: "Masukan Username Anda",
+                hintStyle: TextStyle(color: Colors.black87),
+                labelText: "Username",
+                labelStyle: TextStyle(color: Colors.black87)
+              ),
+             ),
+
+             SizedBox(height: 20,) ,
+
+             TextFormField(
+              obscureText: true,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.black87)
+                ),
+                prefixIcon: Icon(Icons.lock, size: 40,),
+                hintText: "Masukan Password Anda",
+                hintStyle: TextStyle(color: Colors.black87),
+                labelText: "Password",
+                labelStyle: TextStyle(color: Colors.black87)
+              ),
+             ),
+
+             SizedBox(height: 20,) ,
+
+             Card(
+              color: Colors.black87,
+              elevation: 5,
+              child: Container(
+                height: 50,
+                child: InkWell(
+                  splashColor: Colors.white,
+                  onTap: () {},
+                  child: Center(
+                    child: Text("Masuk", style: TextStyle(fontSize: 20, color: Colors.white),),
+                  ),
+                ),
+              ),
+             )
+          ],
+        ),
+      ),
+    );
+  }
+  }
+  
--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,19 @@
 import 'package:flutter/material.dart';
-import 'secondpage.dart';
-
 void main() {
   runApp(myApp());
 }
 
 class myApp extends StatelessWidget {
-  //const myApp({ key? key }) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    final String name = "Nadia Ramadhani";
-    TextEditingController textfortosend = TextEditingController();
-    return Material(
-      initialRouter: '/secondpage',
-      routes: {
-        '/secondpage' :(context) => SecondPage(textfromController: name, textfromTextField: textfortosend.text),
-      },
-
-      home:Scaffold(
-        appBar: AppBar(title: Text("Navigation Router"),),
-        body: Builder(builder: (context)=> Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              TextField(
-                controller: textfortosend,
-                textAlign: TextAlign.center,
-                decoration: InputDecoration(border: InputBorder.none, hintText: "Isi Disini"),
-              ),
-              MaterialButton(onPressed: (){
-                Navigator.push(context, MaterialPageRoute(builder: (context)=>SecondPage()));
-              },
-              color: Colors.blue,
-              child: Text("Pergi ke Halaman 2", style: TextStyle(color: Colors.white))),
-           
-            ],
-          ),
-        )),
+    return MaterialApp(
+      title: 'Login',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        primaryColor: Colors.black
       ),
+      home: Login(),
     );
   }
 }
